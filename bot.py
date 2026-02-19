97438ec7ba48aab62125c04a24bddb906ed9f128
misura088-art <misura088@gmail.com>
2026-02-19 15:57:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 
 BOT_TOKEN = os.getenv("8411217311:AAFqClO0q7BaYdi1mm0I2B_jfV8bL0WRbT8")   # токен бота
-GROUP_ID = int(os.getenv("GROUP_ID")) # ID групи
+GROUP_ID = int(os.getenv("-1003857226732")) # ID групи
 
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
@@ -28,3 +28,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
